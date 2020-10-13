d2227b73b889a523378c209867bdfab87ec8db6d
jeremycbrothers <jeremycbrothers@gmail.com>
2020-10-13 16:14:55 -0400

    Adding some features

--- Assets/scripts/Grid.cs
+++ Assets/scripts/Grid.cs
@@ -1,94 +1,137 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
-using RG.Utils;
 
-public class Grid<TGridObject>
+namespace Room
 {
-    private int width;
-    private int height;
-    private float cellSize;
-    private TGridObject[,] grid;
-    private TextMesh[,] debugTextArray;
-    private Vector3 originPosition;
-
-    public Grid(int width, int height, float cellSize, Vector3 originPosition, Func<TGridObject> createGridObject)
+    public enum CellType
     {
-        this.width = width;
-        this.height = height;
-        this.cellSize = cellSize;
-        this.originPosition = originPosition;
+        e,
+        tg,
+        rg,
+    }
 
-        grid = new TGridObject[width, height];
-        debugTextArray = new TextMesh[width, height];
+    public class Cell
+    {
+        // Used for ground checks   
+        private bool hasTile;
+        private Sprite[] groundSprites;
 
-        for(int x = 0; x < grid.GetLength(0); x++)
+        private GameObject ground;
+
+        public CellType cellType;
+
+        public Cell()
         {
-            for(int y = 0; y < grid.GetLength(1); y++)
+            groundSprites = Resources.LoadAll<Sprite>("tileset-sliced");
+            ground = new GameObject();
+            ground.AddComponent<SpriteRenderer>();
+        }
+
+        public void PlaceTile(Vector3 position, float cellSize)
+        {
+            ground.GetComponent<SpriteRenderer>().sprite = groundSprites[0];
+            
+            BoxCollider2D box = ground.AddComponent<BoxCollider2D>();
+            box.size = new Vector2(cellSize,cellSize);
+            ground.gameObject.transform.position = position + new Vector3(cellSize, cellSize) * 0.5f; // Places tile in the center of the cell
+            hasTile = true;
+        }
+
+        public CellType GetCellType() { return cellType; }
+    }
+
+    public class Grid<TGridObject>
+    {
+        private int width;
+        private int height;
+        private float cellSize;
+        private TGridObject[,] grid;
+        private TextMesh[,] debugTextArray;
+        private Vector3 originPosition;
+
+        public Grid(int width, int height, float cellSize, Vector3 originPosition, Func<TGridObject> createGridObject)
+        {
+            this.width = width;
+            this.height = height;
+            this.cellSize = cellSize;
+            this.originPosition = originPosition;
+
+            grid = new TGridObject[width, height];
+            debugTextArray = new TextMesh[width, height];
+
+            for(int x = 0; x < grid.GetLength(0); x++)
             {
-                grid[x,y] = createGridObject();
+                Debug.Log("here");
+                for(int y = 0; y < grid.GetLength(1); y++)
+                {
+                    grid[x,y] = createGridObject();
+                }
+            }
+
+            //Debugging code
+            // for(int x = 0; x < grid.GetLength(0); x++)
+            // {
+            //     for(int y = 0; y < grid.GetLength(1); y++)
+            //     {
+            //         Debug.Log(grid[x,y].ToString());
+                    
+            //         Debug.DrawLine(GetWorldPosition(x,y), GetWorldPosition(x, y+1), Color.white, 100.0f);
+            //         Debug.DrawLine(GetWorldPosition(x,y), GetWorldPosition(x+1, y), Color.white, 100.0f);
+            //     }
+            // }
+            // Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width,height), Color.white, 100.0f);
+            // Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width,height), Color.white, 100.0f);
+        }
+
+        public TGridObject[,] GetGrid()
+        {
+            return grid;
+        }
+
+        public void SetDebugTextValue(int x, int y, TextMesh text) { debugTextArray[x,y] = text; }
+
+        public void SetValue(int x, int y, TGridObject value)
+        {
+            if(x >= 0 && y >=0 && x < width && y < height)
+            {
+                grid[x, y] = value;
+                debugTextArray[x, y].text = grid[x,y].ToString();
             }
         }
 
-        for(int x = 0; x < grid.GetLength(0); x++)
+        public void SetValue(Vector3 worldPosition, TGridObject value)
         {
-            for(int y = 0; y < grid.GetLength(1); y++)
+            int x, y;
+            GetXY(worldPosition, out x, out y);
+            SetValue(x,y,value);
+        }
+
+        public TGridObject GetValue(int x, int y)
+        {
+            if(x >= 0 && y >=0 && x < width && y < height)
             {
-                Debug.Log(grid[x,y].ToString());
-                //debugTextArray[x,y] = Utils.CreateWorldText(grid[x,y]?.ToString(), null, GetWorldPosition(x,y) + new Vector3(cellSize, cellSize) *0.5f, 20, Color.white, TextAnchor.MiddleCenter);
-                Debug.DrawLine(GetWorldPosition(x,y), GetWorldPosition(x, y+1), Color.white, 100.0f);
-                Debug.DrawLine(GetWorldPosition(x,y), GetWorldPosition(x+1, y), Color.white, 100.0f);
+                return grid[x,y];
             }
+            return default(TGridObject);
         }
-        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width,height), Color.white, 100.0f);
-        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width,height), Color.white, 100.0f);
-    }
 
-    public TGridObject[,] GetGrid()
-    {
-        return grid;
-    }
-
-    public void SetValue(int x, int y, TGridObject value)
-    {
-        if(x >= 0 && y >=0 && x < width && y < height)
+        public TGridObject GetValue(Vector3 worldPosition)
         {
-            grid[x, y] = value;
-            debugTextArray[x, y].text = grid[x,y].ToString();
+            int x,y;
+            GetXY(worldPosition, out x, out y);
+            return GetValue(x,y);
         }
-    }
 
-    public void SetValue(Vector3 worldPosition, TGridObject value)
-    {
-        int x, y;
-        GetXY(worldPosition, out x, out y);
-        SetValue(x,y,value);
-    }
-
-    public TGridObject GetValue(int x, int y)
-    {
-        if(x >= 0 && y >=0 && x < width && y < height)
+        public Vector3 GetWorldPosition(int x, int y)
         {
-            return grid[x,y];
+            return new Vector3(x, y) * cellSize + originPosition;
         }
-        return default(TGridObject);
-    }
 
-    public TGridObject GetValue(Vector3 worldPosition)
-    {
-        int x,y;
-        GetXY(worldPosition, out x, out y);
-        return GetValue(x,y);
-    }
-
-    public Vector3 GetWorldPosition(int x, int y)
-    {
-        return new Vector3(x, y) * cellSize + originPosition;
-    }
-
-    private void GetXY(Vector3 worldPosition, out int x, out int y)
-    {
-        x = Mathf.FloorToInt((worldPosition-originPosition).x / cellSize);
-        y = Mathf.FloorToInt((worldPosition-originPosition).y / cellSize);
+        private void GetXY(Vector3 worldPosition, out int x, out int y)
+        {
+            x = Mathf.FloorToInt((worldPosition-originPosition).x / cellSize);
+            y = Mathf.FloorToInt((worldPosition-originPosition).y / cellSize);
+        }
     }
 }
--- Assets/scripts/RoomGenerator.cs
+++ Assets/scripts/RoomGenerator.cs
@@ -2,56 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 using RG.Utils;
+using Room; // may remove to preserve strong typing
 
 public class RoomGenerator : MonoBehaviour
 {
     private Grid<Cell> grid;
+    private const int width = 8;
+    private const int height = 10;
+    private float cellSize;
+    public Vector3 originPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-       grid = new Grid<Cell>(8, 10, 5.0f, new Vector3(-20,-25,0), () => new Cell());
-       Cell[,] cells = grid.GetGrid();
-       for(int i = 0; i < cells.GetLength(0); i++)
-       {
-           for(int j = 0; j < cells.GetLength(1); j+=2)
-           {
-                Cell cell = (Cell)cells.GetValue(i,j);   
-                cell.PlaceTile(grid.GetWorldPosition(i,j), 5.0f);    
-           }
-       }
-    }
+        // Room template 1
+        Room.CellType[,] CT = new Room.CellType[width, height]
+        {   //Bottom left                                                                                        //Top Left
+            {CellType.tg,CellType.e,CellType.e,CellType.e,CellType.e,CellType.e,CellType.e,CellType.e,CellType.e,CellType.e},
+            {CellType.tg,CellType.e,CellType.e,CellType.rg,CellType.rg,CellType.rg,CellType.rg,CellType.rg,CellType.rg,CellType.e},
+            {CellType.tg,CellType.e,CellType.e,CellType.rg,CellType.rg,CellType.rg,CellType.rg,CellType.rg,CellType.rg,CellType.e},
+            {CellType.tg,CellType.e,CellType.e,CellType.rg,CellType.rg,CellType.rg,CellType.rg,CellType.rg,CellType.rg,CellType.e},
+            {CellType.tg,CellType.e,CellType.e,CellType.rg,CellType.rg,CellType.rg,CellType.rg,CellType.rg,CellType.rg,CellType.e},
+            {CellType.tg,CellType.e,CellType.e,CellType.rg,CellType.rg,CellType.rg,CellType.rg,CellType.rg,CellType.rg,CellType.e},
+            {CellType.tg,CellType.e,CellType.e,CellType.rg,CellType.rg,CellType.rg,CellType.rg,CellType.rg,CellType.rg,CellType.e},
+            {CellType.tg,CellType.e,CellType.e,CellType.e,CellType.e,CellType.e,CellType.e,CellType.e,CellType.e,CellType.e},
+            //Bottom right                                                                                       //Top right
+        };
 
-    // Update is called once per frame
-    private void Update()
-    {
-        if(Input.GetMouseButtonDown(0))
+        cellSize = 5.0f;
+        grid = new Grid<Cell>(width, height, cellSize, originPosition, () => new Cell());
+        Cell[,] cells = grid.GetGrid();
+        // Loop throught the grid of cells and determine which cells get ground tiles.
+        for(int i = 0; i < cells.GetLength(0); i++)
         {
-            Vector3 position = Utils.GetMouseWorldPosition();
-//            grid.SetValue(position, true);
+            for(int j = 0; j < cells.GetLength(1); j++)
+            {
+                // Current cell
+                Cell cell = (Cell)cells.GetValue(i,j);
+                cell.cellType = CT[i,j]; // Set room types to the template
+                if(cell.cellType == CellType.tg) // Place template ground tiles
+                {
+                    cell.PlaceTile(grid.GetWorldPosition(i,j), cellSize);
+                }
+                // If the cell is a random cell. Generate a random number between 0-2 (actual values would be 0 and 1 bc of how the range function works)
+                else if(cell.cellType == CellType.rg)
+                {
+                    int number = Random.Range(0,2);
+                    Debug.Log(number);
+                    if(number==1)
+                    {
+                        cell.PlaceTile(grid.GetWorldPosition(i,j), cellSize);
+                    }
+                }
+
+                // Debug draw room types
+                // grid.SetDebugTextValue(i,j, RG.Utils.Utils.CreateWorldText(
+                //         cells[i,j].GetCellType().ToString(), 
+                //         null, 
+                //         grid.GetWorldPosition(i,j) + new Vector3(cellSize, cellSize) *0.5f, 
+                //         20, 
+                //         Color.white,
+                //         TextAnchor.MiddleCenter
+                //     )
+                // );   
+            }
         }
-    }
-}
-
-public class Cell
-{
-    private bool hasTile;
-    private Sprite[] groundSprites;
-    private GameObject ground;
-
-    public Cell()
-    {
-        groundSprites = Resources.LoadAll<Sprite>("tileset-sliced");
-        ground = new GameObject();
-        ground.AddComponent<SpriteRenderer>();
-    }
-
-    public void PlaceTile(Vector3 position, float cellSize)
-    {
-        ground.GetComponent<SpriteRenderer>().sprite = groundSprites[0];
-        
-        BoxCollider2D box = ground.AddComponent<BoxCollider2D>();
-        box.size = new Vector2(cellSize,cellSize);
-        ground.gameObject.transform.position = position + new Vector3(cellSize, cellSize) *0.5f;
     }
 }
